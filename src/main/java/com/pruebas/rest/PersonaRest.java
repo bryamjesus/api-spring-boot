5429798d22b9e04f272bedd80a759a718efb049a
Bryam Talledo <bryamtalledog@gmail.com>
2022-08-17 11:55:18 -0500

    Metodo GET

--- src/main/java/com/pruebas/rest/PersonaRest.java
+++ src/main/java/com/pruebas/rest/PersonaRest.java
@@ -3,10 +3,9 @@ package com.pruebas.rest;
 import com.pruebas.dao.PersonaDAO;
 import com.pruebas.model.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("personas")
@@ -17,6 +16,11 @@ public class PersonaRest {
     @PostMapping("/guardar")
     public void guardar(@RequestBody Persona persona) {
         personaDAO.save(persona);
+    }
+
+    @GetMapping("/listar")
+    public List<Persona> listar(){
+        return personaDAO.findAll();
     }
 
 
